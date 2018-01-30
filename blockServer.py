753c10debd116e6c97096cd9520142ac852950e8
TheRedMudder <Ron.Dias@outlook.com>
2018-01-29 21:39:05 -0500

    Adding Exit blockServer Thread

--- blockServer.py
+++ blockServer.py
@@ -7,11 +7,22 @@ port= 80
 def processPostData(post=["No Post Data"]):#Print all post data
     for field in post:
         print(field)
+def terminateServer(server):
+    server.shutdown()
 class PostHandler(SimpleHTTPRequestHandler):
     def do_POST(self):
         print("Gathered Post Data:")
         postData=parse_qsl(self.rfile.read(int(self.headers['Content-Length'])).decode("utf-8"))#Read and Decode Post Data
         Thread(target=processPostData, args=[postData]).start()#Send Post Data to Process
         return SimpleHTTPRequestHandler.do_GET(self) #Handle as no Post Request was given
+    def do_GET(self):
+        if self.path.endswith("exit.html"):#Terminate Server in Thread
+            self.send_response(200, 'OK')
+            print("blockServer Terminated")
+            self.wfile.write(bytes("<h1> blockServer Terminated </h1>", 'UTF-8'))
+            Thread(target=terminateServer, args=[server]).start()  # Send Post Data to Process
+        else:
+            return SimpleHTTPRequestHandler.do_GET(self) #Handle as no Post Request was given
 print("Go to http://localhost/ if PORT %s  taken change it on the line bellow" %port)
-TCPServer(("", port), PostHandler).serve_forever()
+server=TCPServer(("", port), PostHandler)
+server.serve_forever()
